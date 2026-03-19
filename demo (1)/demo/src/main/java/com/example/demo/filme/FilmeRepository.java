7cf2b56b2a9cddde565cc334db27273802d41670
rafinhakenzo2004 <145880137+rafinhakenzo2004@users.noreply.github.com>
2026-03-19 18:22:46 -0300

    Add files via upload

---
+++ demo (1)/demo/src/main/java/com/example/demo/filme/FilmeRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.filme;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional
+public interface FilmeRepository extends JpaRepository<Filme, Long> {}
