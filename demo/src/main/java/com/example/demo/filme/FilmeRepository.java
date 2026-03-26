2176d3e65d63464bf26129618c6edfff4a1b4627
rafinhakenzo2004 <145880137+rafinhakenzo2004@users.noreply.github.com>
2026-03-26 18:16:53 -0300

    Delete demo directory

--- demo/src/main/java/com/example/demo/filme/FilmeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.filme;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
-@Repository
-@Transactional
-public interface FilmeRepository extends JpaRepository<Filme, Long> {}
